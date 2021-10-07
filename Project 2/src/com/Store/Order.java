f8411e2cb1eb5732732ed97b62de1a67bf865c8c
Tim Lorgeree <71152544+tlorgeree@users.noreply.github.com>
2021-10-07 13:21:29 -0400

    Order class variables and methods framework

---
+++ Project 2/src/com/Store/Order.java
@@ -0,0 +1,16 @@
+package com.Store;
+
+public class Order{
+    private String ShippingAddress;
+    private String orderedBy;
+    public String getDestination(){
+        //Do thing
+    }
+    public String gerOrder(){
+        //Do thing
+    }
+    public Order(String _shipAddress, String _customer){
+        ShippingAddress = _shipAddress;
+        orderedBy = _customer;
+    }
+}
