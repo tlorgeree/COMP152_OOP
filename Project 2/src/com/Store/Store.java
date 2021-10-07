bf174a090e80e3e4bc26ab2a92a3ee7465ace249
Tim Lorgeree <71152544+tlorgeree@users.noreply.github.com>
2021-10-07 13:17:18 -0400

    Store method framework

--- Project 2/src/com/Store/Store.java
+++ Project 2/src/com/Store/Store.java
@@ -1,7 +1,27 @@
 package com.Store;
 
-public class Store {
-    public static void main(String[] args){
+import java.util.ArrayList;
 
+public class Store{
+    private ArrayList<String> Orders;
+    private ArrayList<String> Customers;
+    public static void main(String[] args){
+    }
+    public void runStore(){
+        //Do thing
+    }
+    public void makeOrder(String _address, String _customer){
+        //Do thing
+    }
+    public Store(){
+    }
+    public void addCustomer(){
+        //Do thing
+    }
+    public String selectCustomer(){
+        //Do thing
+    }
+    public void manageCustomer(String _selected_customer){
+        //Do thing
     }
 }
