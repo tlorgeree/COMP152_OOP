29c1b01225aa1ac4e30eea896d3f99519dd02738
Tim Lorgeree <71152544+tlorgeree@users.noreply.github.com>
2021-10-20 13:51:45 -0400

    Updated lists to take objects as appropriate. Updated Customer managers

--- Project 2/src/com/Store/Store.java
+++ Project 2/src/com/Store/Store.java
@@ -9,13 +9,14 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Store{
-    private ArrayList<String> Orders;
-    private ArrayList<String> Customers;
+    private ArrayList<Order> Orders;
+    private ArrayList<Customer> Customers;
     public static void main(String[] args) throws IOException {
         var currStore = new Store();
         currStore.runStore();
     }
     public void runStore(){
+
         var menu = true;
         while (menu) {
             Scanner menuSelect = new Scanner(System.in);
@@ -25,7 +26,9 @@ public class Store{
             switch (selection)
             {
                 case "1": addCustomer(); break;
-                case "2": selectCustomer(); break;
+                case "2": var _custSelect = selectCustomer();
+                    manageCustomer(_custSelect);
+                    break;
                 case "3": menu = false; break;
             }
         }
@@ -41,24 +44,23 @@ public class Store{
 
         for (var i = 0; i < _max; i++)
         {
-            Customers.add(customerList[i]);
+            Customers.add(new Customer(customerList[i]));
         }
     }
     public void addCustomer(){
         Scanner nameSelect = new Scanner(System.in);
         System.out.println("What is the customer's name?");
         String _custName = nameSelect.nextLine();
-        var newCustomer = new Customer(_custName);
-        Customers.add(newCustomer.getName());
+        Customers.add(new Customer(_custName));
     }
-    public String selectCustomer(){
+    public Customer selectCustomer(){
         //Select a customer
         //return customer
         int _max = Customers.size();
         System.out.println("Please enter an integer from the following list of customers to select:");
         for(var i = 0; i < _max; i++)
         {
-            System.out.println(i + ". " + Customers.get(i));
+            System.out.println(i + ". " + Customers.get(i).getName());
         }
         Scanner custSelect = new Scanner(System.in);
         String _choice = custSelect.nextLine();
@@ -66,7 +68,22 @@ public class Store{
         return Customers.get(_int_choice);
 
     }
-    public void manageCustomer(String _selected_customer){
-        //Do thing
+    public void manageCustomer(Customer _selected_customer){
+        //Start menu loop
+            //add address
+            //make order
+        var menu = true;
+        while (menu) {
+            Scanner menuSelect = new Scanner(System.in);
+            System.out.println("Select from the following options:\n1. Add Addressr"
+                    +"\n2. Make Order \n3. Quit");
+            String selection = menuSelect.nextLine();
+            switch (selection)
+            {
+                case "1": addCustomer(); break;
+                case "2": selectCustomer(); break;
+                case "3": menu = false; break;
+            }
+        }
     }
 }
